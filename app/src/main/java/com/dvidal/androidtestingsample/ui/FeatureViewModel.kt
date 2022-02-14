aa75a4b6ec86919696f1e7ee23b99df9e48f2a6f
diegodcvidal <diego.vidal@home24.de>
2022-02-14 20:30:52 -0300

    + Adding single live event for the project

--- app/src/main/java/com/dvidal/androidtestingsample/ui/FeatureViewModel.kt
+++ app/src/main/java/com/dvidal/androidtestingsample/ui/FeatureViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dvidal.androidtestingsample.domain.FeatureUseCase
+import com.dvidal.androidtestingsample.utils.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -15,7 +16,7 @@ open class FeatureViewModel @Inject constructor(
     private val _states = MutableLiveData<FeatureContract.State>()
     override val states: LiveData<FeatureContract.State> = _states
 
-    private val _events = MutableLiveData<FeatureContract.Event>()
+    private val _events = SingleLiveEvent<FeatureContract.Event>()
     override val events: LiveData<FeatureContract.Event> = _events
 
     override fun invokeAction(action: FeatureContract.Action) {
